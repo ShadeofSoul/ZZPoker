5b81d015f2fd868b8c6faa21d7e38357f1522c13
kostya <nikshorop@gmail.com>
2022-02-16 21:00:07 +0300

    nedrtn

--- main.js
+++ main.js
@@ -1,38 +1,8 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
-// Change image every 2 seconds
-
-// ++++++++++++++++++++++ FOR BURGER ++++++++++++++++++++++==
-// ==========================================================
-
+window.onload = function () {
+  setTimeout(() => {
+    document.body.classList.add("loaded");
+  }, 1000);
+};
 const burger = document.querySelector(".burger");
 const nav = document.querySelector(".menu");
 const links = document.querySelectorAll(".menu li");
@@ -52,3 +22,38 @@ burger.addEventListener("click", () => {
     }
   });
 });
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("slider-dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+// Change image every 2 seconds
+
+// ++++++++++++++++++++++ FOR BURGER ++++++++++++++++++++++==
+// ==========================================================
